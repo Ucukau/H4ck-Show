ab090077a0eefe030bfbc8e3273c2d63410510af
Radita Aditya <ptbp321@gmail.com>
2024-05-23 19:19:37 +0800

    My medsos

---
+++ suport.py
@@ -0,0 +1,19 @@
+#suport.py
+import time
+from colorama import init, Fore, Back, Style
+
+yutub = "RdT Sc"
+ig = "Radita_Aditya"
+fcbk = "Ucil"
+tiktok = "@meoxin6"
+Github = github.com/Ucukau
+
+def suport():
+    print(f'''
+        {Fore.RED}YOUTUBE   == {yutub}
+        {Fore.BLUE}INSTAGRAM == {ig}
+        {Fore.BLUE}FACEBOOK  == {fcbk}
+        {Fore.GREEN}TIKTOK    == {tiktok}
+        
+        
+        ''')
