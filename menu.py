0f67cf87ed738ce223bd36657f5e4f909a8b77f8
Radita Aditya <ptbp321@gmail.com>
2024-05-23 19:03:56 +0800

    MENU LIST

---
+++ menu.py
@@ -0,0 +1,33 @@
+import time 
+from colorama import init, Fore, Back, Style
+
+def icon_main():
+    print(f'''
+     {Fore.BLUE}
+     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    @@*#*#**#*@@@*@**"@*"@**"@*"@***@"$"@”"*@@@@@@@""#@"@
+     @@@####@$$@""@@"@##*@*##@*##@@@@**@@*@@****@@@@@###
+      @@@@@&_@$@&@@{Fore.GREEN}_H_A_C_K__S_H_O_W_{Fore.BLUE}_@#-#$@&__@@@@@#
+       @@@$@$#$@@@@$@@$$@$@@@@@@@@@@@@@@###_@_@_@_@@
+       @$-$&#$(5($7$(6$+5#+&$-_:;_-$&_&$-$&$;"&$&$
+           #@#@##$########$@$######@#@@@@@#@##$_
+           @#$#@$@#$$#@@#$$##@@##$$#@$#**$#@@
+             #######$$$_&&&$#@@@##$$$№#$$**
+{Style.RESET_ALL}
+    ''')
+
+def main_menu():
+    print(f'''
+    []———————————{Fore.GREEN}MENU{Style.RESET_ALL}————————————[]
+    |{Fore.GREEN} ⟩ 1. Hack Screen{Fore.WHITE} {Fore.WHITE}[{Fore.GREEN}✓{Fore.WHITE}]        |
+    |{Fore.RED} ⟩ 2. Error Screen{Fore.WHITE} {Fore.WHITE}[{Fore.GREEN}✓{Fore.WHITE}]       |
+    |                             |
+    | <—————————————————————————> |
+    |                             |
+    |{Fore.BLUE} ⟩ 3. Suport Admin{Fore.WHITE} {Fore.WHITE}[{Fore.GREEN}✓{Fore.WHITE}]       |
+    |{Fore.BLUE} ⟩ 4. Donasi{Style.RESET_ALL} {Fore.WHITE}[{Fore.GREEN}✓{Fore.WHITE}]             |
+    |{Fore.BLUE} ⟩ 5. Tutorial{Style.RESET_ALL} {Fore.WHITE}[{Fore.GREEN}✓{Fore.WHITE}]           |
+    |{Fore.BLUE} ⟩ 6. Keluar{Style.RESET_ALL} {Fore.WHITE}[{Fore.GREEN}✓{Fore.WHITE}]             |
+    ╔══════════════════════════════[]
+    ║{Fore.YELLOW}Pilih Menu {Fore.WHITE}[{Fore.GREEN} 1, 2, 3, 4, 5, 6 {Style.RESET_ALL}]
+    ║''')
