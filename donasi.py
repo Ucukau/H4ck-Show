030aeb929b6c8b3f452bd8bd1b4d0a7542b70bc4
Radita Aditya <ptbp321@gmail.com>
2024-05-23 19:16:23 +0800

    Donasi iin

---
+++ donasi.py
@@ -0,0 +1,25 @@
+#donasi.py
+from colorama import init, Fore, Back, Style
+
+
+def donasi():
+    print(f'''
+        
+        
+        
+        
+        {Fore.RED}[]          []
+        {Fore.GREEN}\____________/{Style.RESET_ALL}
+        
+        
+        
+        
+        
+        
+        {Fore.BLUE}⟩ GOPAY  {Fore.WHITE}==  {Fore.BLUE}+628774481004
+        {Fore.BLUE}⟩ WHATSAPP  {Fore.WHITE}==  {Fore.BLUE}+6287816695079
+  
+        {Fore.YELLOW} YANG DONASI MAKASIH BANGET YAK, MUDAHAN REZEKI NYA LANCAR TERUS..
+        
+        ''')
+        
