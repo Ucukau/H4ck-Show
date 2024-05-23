2c04a63d5f0d01a10344753200e8bf7f86b417dd
Radita Aditya <ptbp321@gmail.com>
2024-05-23 19:22:40 +0800

    Error screen Menu

---
+++ erot.py
@@ -0,0 +1,14 @@
+#erot.py
+import time
+from colorama import init, Fore, Back, Style
+
+
+
+def start():
+   for i in range(1, 2000):
+      print(f"   {Back.RED}  " + "    5  " + "     29 " + "   hx0   " + "      " + "      " + "      " + "      " + "     5ai " * i, end='\n' )
+      print(f" {Back.GREEN}58a    " + "   0   " + "      " + "      " + "      " + "      " + "      " + "      " + "      " * i, end='\n' )
+      print(f" {Back.BLUE}    " + "      " + "      " + "      " + "      " + "      " + "      " + "      " + "      " * i, end='\n' + "    ")
+      print(f" {Back.WHITE} ti   " + " fi     " + "      " + "  0j    " + "      " + "      " + "      " + "      " + "    d  " * i, end='\r' )
+      time.sleep(0.5)
+      print(f" {Back.RED}  s6  " + "      " + "   0089   " + "    o0  " + "      " + "     c " + "    st  " + "      " + " gf     " * i, end='\n' )
